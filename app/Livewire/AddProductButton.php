ee74b6002036b9d558e0db29e5f5203da9f421b1
kubab1el4 <kubab1el4nski@gmail.com>
2024-01-13 18:32:28 +0100

    feat: implement add product functionality, including modal and button

---
+++ app/Livewire/AddProductButton.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class AddProductButton extends Component
+{
+
+    public bool $addProductModal = false;
+
+    public string $name;
+
+    public string $description;
+
+    public string $category;
+
+    public float $price;
+
+    public function render()
+    {
+        return view('livewire.add-product-button');
+    }
+
+    public function save()
+    {
+        $product = new Product;
+        $product->name = $this->name;
+        $product->description = $this->description;
+        $product->price = $this->price;
+        $product->category = $this->category;
+        $product->save();
+        $this->addProductModal = false;
+        $this->dispatch('productCreated');
+    }
+}
