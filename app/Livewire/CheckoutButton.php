b8f23d0f75d86ad38632762b82d1932c8022191d
kubab1el4 <kubab1el4nski@gmail.com>
2024-01-22 20:33:04 +0100

    feat: add checkout button placeholder

---
+++ app/Livewire/CheckoutButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CheckoutButton extends Component
+{
+    public function render()
+    {
+        return view('livewire.checkout-button');
+    }
+}
